c8c2d25bce79647deb498e3569ea707350e88991
Mandy <centawy@outlook.com>
2024-10-31 16:11:43 +0800

    new file:   MyADO/App.config 	new file:   MyADO/MyADO.csproj 	new file:   MyADO/MyADO.sln 	new file:   MyADO/Program.cs 	new file:   MyADO/Properties/AssemblyInfo.cs 	new file:   MyADO/packages.config

---
+++ MyADO/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyADO
+{
+    class Program
+    {
+        const string collectionUri = "https://dev.azure.com/powerbi";
+        const string projectName = "PowerBIClients";
+        const string repoName = "PowerBIClients";
+        const string pat = "64gsO8qa8PrxgdhTlLKSFHRdSD8YCKy1LCfSvSIzFzAPcSjzWPkkJQQJ99AJACAAAAAAArohAAASAZDODB5c";
+
+        static void Main(string[] args)
+        {
+            var creds = new Microsoft.VisualStudio.Services.Common.VssBasicCredential(string.Empty, pat);
+
+            // Connect to Azure DevOps Services
+            var connection = new VssConnection(new Uri(collectionUri), creds);
+
+            // Get a GitHttpClient to talk to the Git endpoints
+            var gitClient = connection.GetClient<GitHttpClient>();
+
+            // Get data about a specific repository
+            var repo = gitClient.GetRepositoryAsync(projectName, repoName).Result;
+
+            var branch = repo.DefaultBranch;
+
+            Console.WriteLine(branch);
+        }
+    }
+}
